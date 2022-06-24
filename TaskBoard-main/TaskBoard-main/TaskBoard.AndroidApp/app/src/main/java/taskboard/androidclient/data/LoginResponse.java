7457925975d20a1841e39db745229a89ea9552b6
martinrivanov <martinrivanov43@gmail.com>
2022-06-24 15:27:20 +0300

    Removal

--- TaskBoard-main/TaskBoard-main/TaskBoard.AndroidApp/app/src/main/java/taskboard/androidclient/data/LoginResponse.java
+++
@@ -1,7 +0,0 @@
-package taskboard.androidclient.data;
-
-public class LoginResponse {
-    private String token;
-
-    public String getToken() { return token; }
-}
--- TaskBoard-main/TaskBoard-main/TaskBoard.DesktopApp/Data/Board.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TaskBoard.DesktopApp.Data
-{
-    public class Board
-    {
-        public int Id { get; init; }
-        public string Name { get; init; }
-    }
-}
