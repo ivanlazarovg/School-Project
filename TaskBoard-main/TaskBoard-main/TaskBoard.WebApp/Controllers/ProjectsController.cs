c9be9edd7a259c7347959952a90d0cdc455208b6
KoSioA <metabot@abv.bg>
2022-06-24 15:37:28 +0300

    more work on projects

---
+++ TaskBoard-main/TaskBoard-main/TaskBoard.WebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+using TaskBoard.Data;
+using TaskBoard.WebApp.Models;
+using TaskBoard.WebApp.Models.Task;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace TaskBoard.WebApp.Controllers
+{
+    [Authorize]
+    public class ProjectsController : Controller
+    {
+
+        private readonly ApplicationDbContext dbContext;
+        public ProjectsController(ApplicationDbContext context)
+        {
+            this.dbContext = context;
+        }
+
+
+        public IActionResult All()
+        {
+            var projects = this.dbContext.Projects
+                .Select(p => new ProjectViewModel()
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Description = p.Description
+                });
+            return View(projects);
+        }
+    }
+}
